5a4e56432e5bd79d1324ccdd60b9d939c3cfddc5
tetramputechture <tetramputechture@gmail.com>
2024-12-26 17:27:04 -0700

    Remove unused image

--- npp_rl/util/util.py
+++ npp_rl/util/util.py
@@ -12,23 +12,3 @@ def calculate_distance(x1: float, y1: float, x2: float, y2: float) -> float:
             float: Euclidean distance between the points
         """
     return np.sqrt((x2 - x1)**2 + (y2 - y1)**2)
-
-
-def calculate_velocity(x, y, prev_x, prev_y, timestep):
-    """Calculate velocity components from position changes over a timestep.
-
-    Args:
-        x, y: Current position
-        prev_x, prev_y: Previous position
-        timestep: Time step
-
-    Returns:
-        Tuple[float, float]: Velocity components (vx, vy)
-    """
-    dx = x - prev_x
-    dy = y - prev_y
-
-    vx = dx / timestep
-    vy = dy / timestep
-
-    return vx, vy
